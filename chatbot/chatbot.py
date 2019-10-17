7baae9fa02cd6b93815f6e4bd4bef853cc83f989
wendelanchieta <wendelanchieta@gmail.com>
2019-10-17 00:29:06 -0300

    Limpeza das perguntas e respostas

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -70,21 +70,42 @@ for conversa in conversas_id:
 def limpa_texto(texto):
     texto = texto.lower()
     texto = re.sub(r"i'm", "i am", texto)
+    texto = re.sub(r"i ' m", "i am", texto)
     texto = re.sub(r"he's", "he is", texto)
+    texto = re.sub(r"he ' s", "he is", texto)
     texto = re.sub(r"she's", "she is", texto)
+    texto = re.sub(r"she ' s", "she is", texto)
     texto = re.sub(r"that's", "that is", texto)
+    texto = re.sub(r"that ' s", "that is", texto)
     texto = re.sub(r"what's", "what is", texto)
+    texto = re.sub(r"what ' s", "what is", texto)
     texto = re.sub(r"where's", "where is", texto)
+    texto = re.sub(r"where ' s", "where is", texto)
     texto = re.sub(r"\'ll", " will", texto)
+    texto = re.sub(r"\ ' ll", " will", texto)
     texto = re.sub(r"\'ve", " have", texto)
+    texto = re.sub(r"\ ' ve", " have", texto)
     texto = re.sub(r"\'re", " are", texto)
+    texto = re.sub(r"\ ' re", " are", texto)
     texto = re.sub(r"\'d", " would", texto)
+    texto = re.sub(r"\ ' d", " would", texto)
     texto = re.sub(r"won't", "will not", texto)
+    texto = re.sub(r"won ' t", "will not", texto)
     texto = re.sub(r"can't", "cannot", texto)
+    texto = re.sub(r"can ' t", "cannot", texto)
     texto = re.sub(r"[-()#/@;:<>~{}~+=.|,%¨]", "", texto)
+    texto = re.sub(r"  ", " ", texto)
     return texto
 
-limpa_texto("ExeMplo i'm #@ -=+|?")
+# Limpeza das perguntas
+perguntas_limpas = []
+for pergunta in perguntas:
+    perguntas_limpas.append(limpa_texto(pergunta))    
+
+# Limpeza das respostas
+respostas_limpas = []    
+for resposta in respostas:
+    respostas_limpas.append(limpa_texto(resposta))    
 
 
 
