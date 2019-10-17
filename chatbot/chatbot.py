537b85e68e296996a0a0f41a4453ab1cf98c5119
wendel <wendel@localhost.localdomain>
2019-10-16 23:39:47 -0300

    Criação da função para limpar caracteres especiais e contrações de texto

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -67,9 +67,24 @@ for conversa in conversas_id:
         perguntas.append(id_para_linha[conversa[i]])
         respostas.append(id_para_linha[conversa[i+1]])
 
+def limpa_texto(texto):
+    texto = texto.lower()
+    texto = re.sub(r"i'm", "i am", texto)
+    texto = re.sub(r"he's", "he is", texto)
+    texto = re.sub(r"she's", "she is", texto)
+    texto = re.sub(r"that's", "that is", texto)
+    texto = re.sub(r"what's", "what is", texto)
+    texto = re.sub(r"where's", "where is", texto)
+    texto = re.sub(r"\'ll", " will", texto)
+    texto = re.sub(r"\'ve", " have", texto)
+    texto = re.sub(r"\'re", " are", texto)
+    texto = re.sub(r"\'d", " would", texto)
+    texto = re.sub(r"won't", "will not", texto)
+    texto = re.sub(r"can't", "cannot", texto)
+    texto = re.sub(r"[-()#/@;:<>~{}~+=.|,%¨]", "", texto)
+    return texto
 
-
-
+limpa_texto("ExeMplo i'm #@ -=+|?")
 
 
 
