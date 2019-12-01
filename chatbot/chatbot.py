76e8ebbc65433979f86930eb0521d97088726e63
Wendel anchieta <wendelanchieta@gmail.com>
2019-12-01 21:40:43 -0200

    Definição da seção

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -360,7 +360,9 @@ learning_rate_decaimento = 0.9
 min_learning_rate = 0.0001
 probabilidade_dropout = 0.5
 
-
+# Definição da seção
+tf.reset_default_graph()
+session = tf.InteractiveSession()
 
 
     
